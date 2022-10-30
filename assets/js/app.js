e5790ad5619389008a1bcdea5b49f63c5b8579fb
Vishal Gaikwad <gaikwad@gmail.com>
2022-10-30 11:41:45 +0530

    boostrap-1

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+
+let cl = console.log;
+
+let pi = Math.PI;
+cl(pi)
+
+let x = 1.6;
+
+let round = Math.round(x);
+cl(round)
+
+
+let xx = 1.46;
+
+xx = Math.round(xx)
+
+
+let yy = Math.floor(xx)
+cl(yy)
+
